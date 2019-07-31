f51409412226e16351519055f9e25d7e659cfce5
zyh-hust <zhouyuanhui@pingcap.com>
2019-07-31 20:40:59 +0800

    zhouyuanhui@pingcap.com
    Signed-off-by: zyh-hust <zhouyuanhui@pingcap.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use client::Client;
 use grpcio::EnvBuilder;
 use std::sync::Arc;
 use std::thread::{self, JoinHandle};
+use std::time::Duration;
 
 fn main() {
     let env = Arc::new(EnvBuilder::new().build());
@@ -10,20 +11,23 @@ fn main() {
 
     // get the regions
     let start_key = "a".to_string().into_bytes();
-    client.scan_regions(start_key);
+    if client.scan_regions(start_key) {
+        println!("scan end!");
+    }
 
     let mut workers: Vec<JoinHandle<()>> = Vec::new();
     for i in 0..4 {
         let mut client = client.clone();
         let t = thread::spawn(move || {
-            let head = match i {
-                0 => "a",
-                1 => "b",
-                2 => "c",
-                3 => "d",
-                _ => "a",
+            let (head, split) = match i {
+                0 => ("a", "b"),
+                1 => ("b", "c"),
+                2 => ("c", "d"),
+                3 => ("d", "e"),
+                _ => ("a", "b"),
             };
 
+            let head = "z";
             let mut count = 1;
             loop {
                 let key = format!("{:?}-{}", head, count).into_bytes();
@@ -31,8 +35,9 @@ fn main() {
                 let resp = client.raw_put(key, value);
                 println!("put count: {:?} resp: {:?}", count, resp);
                 count += 1;
-                if count == 100000000 {
+                if count == 1000000 {
                     count = 0;
+                    thread::sleep(Duration::from_secs(10));
                 }
             }
         });
